5ccba9664c2cacda859450b3dda7d5a244c9512c
Dhanush Ekollu <66331423+DEboy2007@users.noreply.github.com>
2021-06-20 13:57:06 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands, tasks
 import motion_detection
 from datetime import datetime
 
-TOKEN = "TOKEN_HERE"
+TOKEN = "INSERT TOKEN HERE"
 
 help_message = """```
 arm - Turn on security notifications
@@ -31,7 +31,7 @@ async def info(ctx):
 
 @client.command()
 async def update(ctx):
-    await ctx.send("Current view outside the house (Looks great huh)", file=discord.File("current_image.jpg"))  # Just a test image for now
+    await ctx.send("Current view outside the house (Looks great huh)", file=discord.File("current_image.jpg"))
 
 
 @client.command()
@@ -49,13 +49,13 @@ async def disarm(ctx):
 
 @client.command()
 async def status(ctx):
-    await ctx.send("Security system is {}\nTime: {}".format(armed, datetime.now()))
+    await ctx.send("Security system is currently {}\nTime: {}".format(armed, datetime.now()))
 
 
-@tasks.loop(seconds=15)
+@tasks.loop(seconds=3)
 async def detect_motion():
-    channel = client.get_channel(INSERT CHANNEL ID)
-    if motion_detection.detect_motion() and armed == "armed":
+    channel = client.get_channel(0000) # Replace with channel ID where you want alerts to show up
+    if motion_detection.Main() and armed == "armed":
         await channel.send("POSSIBLE INTRUDER ALERT")
 
 
